5a295edac3ba1727ff611db4ce8be26972233af3
Alex Hinds <alex.hinds@foxsports.com.au>
2018-09-20 10:18:52 +1000

    Just an old commit I forgot to push

--- labs/basics/objects.js
+++ labs/basics/objects.js
@@ -1,18 +1,18 @@
 /*
- * given a name and a age make a person object
+ * given a name and a age make a Dog object
  * which stores this information
  * and which has a function called
  * can_drink which returns true of the
- * person is >= 18 and false otherwise
+ * Dog is >= 18 and false otherwise
  *
- * const me = makePerson("sam",91)
+ * const me = makeDog("sam",91)
  * me.name should be "sam"
  * me.age should be 91
  * me.can_drink() should be true
  */
 
-function Person(name, age) {
+function Dog(name, age) {
     console.log(name, age);
 }
 
-module.exports = Person;
+module.exports = Dog;

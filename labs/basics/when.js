6d6a7fcd5c358244b60bd2d1dcf586f2e2ab39f0
Zain Afzal <zain.afz@gmail.com>
2018-10-07 23:53:18 +1100

    added challange

---
+++ labs/basics/when.js
@@ -0,0 +1,13 @@
+/*
+ * Given a input string from a user
+ * return a js datetime object that
+ * matches the day/time the user is describing
+ *
+ * Level 1:
+ */
+
+function identify (string) {
+  
+}
+
+module.exports = identify;
